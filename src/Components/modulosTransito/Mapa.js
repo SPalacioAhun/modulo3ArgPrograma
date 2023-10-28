f9ad65bbb8e2ccf588e4602a609882aebaa63add
SPalacioAhun <spa497@gmail.com>
2023-10-28 13:18:57 -0300

    completando transito

--- src/Components/modulosTransito/Mapa.js
+++ src/Components/modulosTransito/Mapa.js
@@ -1,121 +1,65 @@
-import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet'
 
-import Transp from './transp.json'
+import React, { useState, useEffect } from 'react';
+import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet';
 import L from 'leaflet';
+import TranspJson from '../../Data/transp.json';
 
-// iconos personalizados: 
-const customIcon153 = new L.Icon({
-  iconUrl: 'https://www.xcolectivo.com.ar/imagenes/colectivos/identificador/linea153.jpg', // Reemplaza 'URL_DEL_ICONO' con la URL de tu icono personalizado
-  iconSize: [25, 41], // Tamaño del icono
-  iconAnchor: [12, 41], // Punto de anclaje del icono
-});
+const routeIconsF = (numLinea) => {
+  return `https://www.xcolectivo.com.ar/imagenes/colectivos/identificador/linea${numLinea}.jpg`;
+}
 
-const customIcon321 = new L.Icon({
-  iconUrl: 'https://www.xcolectivo.com.ar/imagenes/colectivos/identificador/linea321.jpg', // Reemplaza 'URL_DEL_ICONO' con la URL de tu icono personalizado
-  iconSize: [25, 41], // Tamaño del icono
-  iconAnchor: [12, 41], // Punto de anclaje del icono
-});
-
-const customIcon159 = new L.Icon({
-  iconUrl: 'https://www.xcolectivo.com.ar/imagenes/colectivos/identificador/linea159.jpg', // Reemplaza 'URL_DEL_ICONO' con la URL de tu icono personalizado
-  iconSize: [25, 41], // Tamaño del icono
-  iconAnchor: [12, 41], // Punto de anclaje del icono
-});
-
-const customIcon148 = new L.Icon({
-  iconUrl: 'https://www.xcolectivo.com.ar/imagenes/colectivos/identificador/linea148.jpg', // Reemplaza 'URL_DEL_ICONO' con la URL de tu icono personalizado
-  iconSize: [25, 41], // Tamaño del icono
-  iconAnchor: [12, 41], // Punto de anclaje del icono
-});
-
-
-
-function Mapa({ loading, transportData }) {
-
-  // Para buscar casos que contengan "153" en route_short_name
-  const linea153 = Transp.filter((obj) => /153/.test(obj.route_short_name));
-  const linea321 = Transp.filter((obj) => /321/.test(obj.route_short_name));
-  const linea159 = Transp.filter((obj) => /159/.test(obj.route_short_name));
-  const linea148 = Transp.filter((obj) => /148/.test(obj.route_short_name));
-
-  // Combinar todas las lineas en una sola lista para poder mostar todas en el mapa al mismo tiempo
-  const lineasCombinadas = [...linea153, ...linea321, ...linea159, ...linea148];
-
-  /*   console.log(linea153A)
-    console.log(linea321A)
-    console.log(linea159)
-    console.log(lineasCombinadas)
-   */
+function CustomMarker({ data, iconUrl }) {
+  const { latitude, longitude, route_short_name, trip_headsign, speed, agency_name, agency_id, route_id } = data;
+  const position = [latitude, longitude];
 
   return (
+    <Marker position={position} icon={new L.Icon({ iconUrl, iconSize: [25, 41], iconAnchor: [12, 41] })}>
+      <Popup>
+        <span>Linea: {route_short_name}</span><br />
+        <span>Dirección de la Ruta: {trip_headsign}</span><br />
+        <span>Velocidad Actual: {speed}</span><br />
+        <span>Nombre de la Empresa: {agency_name}. Cod. Empresa: {agency_id}</span><br />
+        <span>ID de la Ruta: {route_id}</span><br />
+      </Popup>
+    </Marker>
+  );
+}
 
+function Mapa({ loading, transportData }) {
+  const [selectedLine, setSelectedLine] = useState('');
+  const [markers, setMarkers] = useState([]);
 
-    <>
-      
-      <MapContainer center={[lineasCombinadas[0].latitude, lineasCombinadas[0].longitude]} zoom={10} style={{ height: '500px', width: '100%' }}>
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        />
-        {linea153.map((item, index) => {
-          const position = [item.latitude, item.longitude];
-          return (
-            <Marker key={index} position={position} icon={customIcon153}>
-              <Popup>
-                <p>Linea: {item.route_short_name}</p>
-                <p>Dirección de la Ruta: {item.trip_headsign}</p>
-                <p>Velocidad Actual: {item.speed}</p>
-                <p>Nombre de la Empresa: {item.agency_name}. Cod. Empresa: {item.agency_id}</p>
-                <p>ID de la Ruta: {item.route_id}</p>
-              </Popup>
-            </Marker>
-          );
-          
-        })}
-        {linea321.map((item, index) => {
-          const position = [item.latitude, item.longitude];
-          return (
-            <Marker key={index} position={position} icon={customIcon321}>
-              <Popup>
-                <p>Linea: {item.route_short_name}</p>
-                <p>Dirección de la Ruta: {item.trip_headsign}</p>
-                <p>Velocidad Actual: {item.speed}</p>
-                <p>Nombre de la Empresa: {item.agency_name}. Cod. Empresa: {item.agency_id}</p>
-                <p>ID de la Ruta: {item.route_id}</p>
-              </Popup>
-            </Marker>
-          );
-        })}
-        {linea159.map((item, index) => {
-          const position = [item.latitude, item.longitude];
-          return (
-            <Marker key={index} position={position} icon={customIcon159}>
-              <Popup>
-                <p>Linea: {item.route_short_name}</p>
-                <p>Dirección de la Ruta: {item.trip_headsign}</p>
-                <p>Velocidad Actual: {item.speed}</p>
-                <p>Nombre de la Empresa: {item.agency_name}. Cod. Empresa: {item.agency_id}</p>
-                <p>ID de la Ruta: {item.route_id}</p>
-              </Popup>
-            </Marker>
-          );
-        })}
-        {linea148.map((item, index) => {
-          const position = [item.latitude, item.longitude];
-          return (
-            <Marker key={index} position={position} icon={customIcon148}>
-              <Popup>
-                <p>Linea: {item.route_short_name}</p>
-                <p>Dirección de la Ruta: {item.trip_headsign}</p>
-                <p>Velocidad Actual: {item.speed}</p>
-                <p>Nombre de la Empresa: {item.agency_name}. Cod. Empresa: {item.agency_id}</p>
-                <p>ID de la Ruta: {item.route_id}</p>
-              </Popup>
-            </Marker>
-          );
-        })}
-      </MapContainer>
-    </>
+  // Función para actualizar las posiciones de los marcadores
+  const updateMarkers = () => {
+    const filteredData = !loading ? transportData : [];
+    const filteredDataByLine = filteredData.filter((item) => {
+      const routeName = item.route_short_name.match(/\d+/);
+      return selectedLine === '' || routeName.toString() === selectedLine;
+    });
+    setMarkers(filteredDataByLine);
+  };
+
+  // Llamar a la función para actualizar los marcadores en un intervalo de 30 segundos
+  useEffect(() => {
+    updateMarkers();
+    const interval = setInterval(() => {
+      updateMarkers();
+    }, 30000);
+    return () => clearInterval(interval);
+  }, [selectedLine, loading, transportData]);
+
+  return (
+    <MapContainer center={[-34.60376, -58.38162]} zoom={10.5} style={{ height: '500px', width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {markers.map((item, index) => {
+        const routeName = item.route_short_name.match(/\d+/);
+        const iconUrl = routeIconsF(routeName);
+        return <CustomMarker key={index} data={item} iconUrl={iconUrl} />;
+      })}
+    </MapContainer>
   );
 }
 
--- src/Components/modulosTransito/TransporteApp.js
+++ src/Components/modulosTransito/TransporteApp.js
@@ -1,6 +1,8 @@
+
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Mapa from './Mapa';
+import Conversion from '../../Data/conversion.json';
 
 const ContenedorGral = styled.div`
   display: block;
@@ -9,35 +11,46 @@ const ContenedorGral = styled.div`
 function TransitoDashboard() {
   const [transportData, setTransportData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [selectedLine, setSelectedLine] = useState('');
 
-  const apiUrl = "https://apitransporte.buenosaires.gob.ar/colectivos/vehiclePositionsSimple?client_id=cb6b18c84b3b484d98018a791577af52&client_secret=3e3DB105Fbf642Bf88d5eeB8783EE1E6";
+  const fetchdata = (idRuta) => {
+    const apiUrl = `https://apitransporte.buenosaires.gob.ar/colectivos/vehiclePositionsSimple?route_id=${idRuta}&client_id=cb6b18c84b3b484d98018a791577af52&client_secret=3e3DB105Fbf642Bf88d5eeB8783EE1E6`;
+
+    setLoading(true);
+    fetch(apiUrl)
+      .then((resp) => resp.json())
+      .then((data) => {
+        setTransportData(data);
+        setLoading(false);
+      })
+      .catch((ex) => {
+        console.error(ex);
+      });
+  }
 
   useEffect(() => {
-    const fetchData = () => {
-      setLoading(true);
-      fetch(apiUrl)
-        .then((resp) => resp.json())
-        .then((data) => {
-          setTransportData(data);
-          setLoading(false);
-        })
-        .catch((ex) => {
-          console.error(ex);
-          setLoading(false); // En caso de error, también detenemos la carga
-        });
-    };
-
-    fetchData(); // Llamamos a la función para cargar los datos al inicio
-    const interval = setInterval(fetchData, 31000);
-
+    fetchdata(Conversion[selectedLine]);
+    const interval = setInterval(() => {
+      fetchdata(Conversion[selectedLine]);
+    }, 31000);
     return () => clearInterval(interval);
-  }, []);
+  }, [selectedLine]);
 
   return (
     <ContenedorGral>
       <h1>Colectivos de CABA</h1>
+
+      {/* Desplegable para seleccionar una línea */}
+      <select value={selectedLine} onChange={(e) => setSelectedLine(e.target.value)}>
+        <option value="">Seleccione una opción</option>
+        {Object.keys(Conversion).map((routeName, index) => (
+          <option key={index} value={routeName}>
+            {routeName}
+          </option>
+        ))}
+      </select>
+      <p></p>
       <Mapa transportData={transportData} loading={loading} />
-      {loading && <h2>Cargando</h2>}
     </ContenedorGral>
   );
 }
